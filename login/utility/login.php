2259b5ae562e1fc58fbdb0ebdd0d97af04cb061a
federicodimaio <fed.dimaio@gmail.com>
2023-04-03 21:36:00 +0200

    fix

--- login/utility/login.php
+++ login/utility/login.php
@@ -7,40 +7,40 @@ CONNESIONE PDO
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="progettoUno"
+$dbname="progettoUno";
 
 try{
     $db = new PDO("mysql:=$servername;dbname=$dbname", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+}catch (PDOException $e) {
     print "ERRORE!: ". $e->getMessage() . "<br>";
     die();
 }
 
 /*------------------------------
-LOGIN
+LOGIN tet@test.com
 -------------------------------*/
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+
 //query
-$q = $db->prepare("SELECT * FROM utenti WHERE email = '$email'");
+$q = $db->prepare("SELECT * FROM utente WHERE email = '$email'");
 $q->execute();
 $q->setFetchMode(PDO::FETCH_ASSOC);   //fetchiamo e passiamo a rassegna tutte le righe
 $rows = $q->rowCount();               //contiamo righe
 if($row>0){                           //utente esiste
     while ($row=$q->fetch()){
         if($row['password']===$password){
-            echo "LOGIN CORRETTO"
+            echo "LOGIN CORRETTO";
         }else{
-            echo "PASSWORD ERRATA"
+            echo "PASSWORD ERRATA";
         }
-        echo "utente trovato in archivio";
-
-    }
+    }   
 }else{
     echo "utente non trovato in archivio";
 }
+
 
 ?>
