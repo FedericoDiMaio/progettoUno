7c57a18debfac06404796b3b13df93199b980c81
federico di maio <f.dimaio@cineca.it>
2023-05-06 09:53:17 +0200

    06-05-23

---
+++ addressingmarconi/newStart.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basi Di Dati</title>
+<link rel="stylesheet" href="./newStart.css">
+</head>
+<body>
+	<header>
+		<img aria-hidden="true" alt="Logo ateneo" src="img/layout/logo.png?v=22.09.02" id="theme-logo">
+		<h1>Progetti Basi Di Dati</h1>
+	</header>
+	<div class="container">
+		<div class="project">
+			<img src="progetto-uno.jpg" alt="Progetto Uno">
+			<a href= "http://localhost/progettoUno/addressingmarconi/trainstation/landing.php"><h2>Progetto Uno</h2>
+            <a href="http://localhost/progettoUno/addressingmarconi/trainstation/landing.php">PROGETTO UNO</a>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisl vel massa convallis sagittis non ut nunc. Nullam eget iaculis sapien. Morbi lacinia dolor quis purus malesuada, eu cursus lectus sodales. Proin eget eros augue. Fusce ac quam ut mauris porttitor venenatis in a elit.</p>
+			<p><strong>Informazioni aggiuntive:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisl vel massa convallis sagittis non ut nunc. Nullam eget iaculis sapien. Morbi lacinia dolor quis purus malesuada, eu cursus lectus sodales.</p>
+		</div>
+		<div class="project">
+			<img src="progetto-due.jpg" alt="Progetto Due">
+			<h2>Progetto Due</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisl vel massa convallis sagittis non ut nunc. Nullam eget iaculis sapien. Morbi lacinia dolor quis purus malesuada, eu cursus lectus sodales. Proin eget eros augue. Fusce ac quam ut mauris porttitor venenatis in a elit.</p>
+			<p><strong>Informazioni aggiuntive:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisl vel massa convallis sagittis non ut nunc. Nullam eget iaculis sapien. Morbi lacinia dolor quis purus malesuada, eu cursus lectus sodales.</p>
+		</div>
+	</div>
+</body>
+</html>
---
+++ addressingmarconi/trainstation/registrazione/registrazione.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Registrazione utente</title>
+    <link rel="stylesheet" href="./registrazione.css">
+  </head>
+  <body>
+    <header>
+      <h1>Registrazione utente</h1>
+    </header>
+    <form>
+      <div class="form-row">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+      </div>
+      <div class="form-row">
+        <label for="cognome">Cognome:</label>
+        <input type="text" id="cognome" name="cognome" required>
+      </div>
+      <div class="form-row">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+      </div>
+      <div class="form-row">
+        <label for="telefono">Numero di telefono:</label>
+        <input type="tel" id="telefono" name="telefono" required>
+      </div>
+      <div class="form-row">
+        <label for="residenza">Residenza:</label>
+        <input type="text" id="residenza" name="residenza" required>
+      </div>
+      <div class="form-row">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+      </div>
+      <div class="form-row">
+        <label for="conferma_password">Conferma password:</label>
+      <input type="password" id="conferma_password" name="conferma_password" required>
+    </div>
+    <div class="form-row">
+      <label for="ruolo">Ruolo:</label>
+      <select id="ruolo" name="ruolo" required>
+        
+        <option value="registrato">registrato</option>
+      </select>
+    </div>
+    <div class="form-row">
+      <input type="submit" value="Registrati">
+    </div>
+  </form>
+</body>
